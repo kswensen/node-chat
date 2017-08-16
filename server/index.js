d8c7045c3da5f5714e18cdfcf44c9ce2ac44ab54
StayinNasty <keganswensen@gmail.com>
2017-08-16 15:49:33 -0600

    Finished

---
+++ server/index.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const mc = require(__dirname + '/controllers/messages_controller.js');
+const app = express();
+app.use(bodyParser.json());
+app.use(express.static(__dirname + '/../public/build'));
+
+const apiURL = "/api/messages";
+app.get(apiURL, mc.read);
+app.post(apiURL, mc.create);
+app.put(`${apiURL}/:id`, mc.update);
+app.delete(`${apiURL}/:id`, mc.delete);
+
+
+
+const port = 3000;
+app.listen(port, ()=> {console.log(`Its lit on port ${port} fam`)});
